68c1a1b89c93039bd46f00092821310b2b42073d
Kartikshukla17 <kartikconnect17@gmail.com>
2025-03-01 23:08:24 +0530

    deploy issue fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 module.exports = {
     content: [
       "./app/**/*.{js,ts,jsx,tsx}",
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
+      //"./pages/**/*.{js,ts,jsx,tsx}",
+      //"./components/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
       extend: {},
     },
     plugins: [
-      require("@tailwindcss/forms"), // If you're using Tailwind Forms
+      //require("@tailwindcss/forms"), // If you're using Tailwind Forms
     ],
   };
